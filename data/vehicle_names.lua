fc6d658cb78ff9ada0c448128b8b846e9f2b5d63
mark <43683580+markbucko12341@users.noreply.github.com>
2019-06-09 06:53:25 +0100

    Initial commit

---
+++ data/vehicle_names.lua
@@ -0,0 +1,17 @@
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	-- cvpi w/ liberty
+	AddTextEntry('0x54830233', 'SHERIFF11')
+	-- 2015 charger
+	AddTextEntry('0x519EF9F7', 'SHERIFF12')
+	-- 2014 explorer
+	AddTextEntry('0x43605D7A', 'SHERIFF13')
+	-- 2014 slicktop explorer
+	AddTextEntry('0x61A019F9', 'SHERIFF14')
+	-- ford expedition
+	AddTextEntry('0xD3487D4C', 'SHERIFF15')
+
+
+
